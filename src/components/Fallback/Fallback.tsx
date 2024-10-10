c964c08002c7d761ed1ec1e8e73b9ae6a0395dfe
ibabkov <i.babkov.dev@gmail.com>
2024-10-09 23:59:02 -0400

    feat: add an advice

--- src/components/Fallback/Fallback.tsx
+++ src/components/Fallback/Fallback.tsx
@@ -11,6 +11,10 @@ export const Fallback: React.FC<FallbackProps> = props => {
 
 	return (
 		<div className={styles.container}>
+			<span className={styles.advice}>
+				<span className={styles['advice-title']}>Advice</span>
+				<span className={styles['advice-text']}>You can ROTATE the model</span>
+			</span>
 			<span className={styles.loader}>Loading</span>
 			{loadingProgress >= 0 && (
 				<span className={styles.progress}>
